fc6de4493363085790ebec6b5af69d4c34306025
plumbumtroll <speacenows@mail.ru>
2023-02-19 16:56:48 +0400

    Created Class for data storaging about dices sets

---
+++ src/SetsStorage.java
@@ -0,0 +1,17 @@
+public class SetsStorage {
+
+    int value;
+    int count;
+    SetsStorage() {
+        value = 0;
+        count = 0;
+    }
+    SetsStorage(int value, int count) {
+        this.value =  value;
+        this.count = count;
+    }
+}
+
+
+//    sixDiceSet ()
+
